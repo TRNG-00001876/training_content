2be33c8c16ee60740f8400d5f0c04291c97824ef
kaveripriya <kaveri.priya@revature.com>
2024-05-10 09:58:52 +0530

    Added Projects in angular

---
+++ Week_5/Angular_projects/demo_project/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {HelloTeamComponent} from './hello-team.component';
+import { ContactInfoComponent } from './contact-info/contact-info.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HelloTeamComponent,
+    ContactInfoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Week_5/Angular_projects/demo_project/src/app/hello-team.component.ts
@@ -0,0 +1,10 @@
+import {Component} from'@angular/core'
+
+@Component({
+    selector:'app-hello-team',
+    templateUrl:'./hello-team.component.html',
+    styleUrls:['./hello-team.component.css']
+})
+export class HelloTeamComponent{
+    title='Revature_Randstad Java Cohort';
+}
